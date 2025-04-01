bb43f05652fbc19c11cf8cdce0bd9bab52f57649
RowanWA <Rowan.Armitage@student.norwichuni.ac.uk>
2025-04-01 13:29:00 +0100

    Add files via upload

---
+++ sketch_apr1a.ino
@@ -0,0 +1,57 @@
+// #include "pitches.h"
+
+// constexpr int piezoPin = 13;
+// constexpr int LedPin_1 = 6;
+// constexpr int LedPin_2 = 7;
+
+
+// void setup() {
+//   pinMode(LedPin_1, HIGH);
+//   pinMode(LedPin_2, HIGH);
+// }
+// void loop() {
+
+//   tone(piezoPin, NOTE_C6, 1000);
+//   digitalWrite(LedPin_1, HIGH);
+
+//   delay (1000);
+
+//   noTone(piezoPin);
+//   digitalWrite(LedPin_1, LOW);
+
+//   delay (1000);
+
+//   tone(piezoPin, NOTE_C1, 1000);
+//   digitalWrite(LedPin_2, HIGH);
+
+//   delay (1000);
+
+//   noTone(piezoPin);
+//   digitalWrite(LedPin_2, LOW);
+
+//   delay (1000);
+
+
+//   tone(piezoPin, NOTE_G2, 1000);
+//   digitalWrite(LedPin_1, HIGH);
+
+//   delay (1000);
+
+//   noTone(piezoPin);
+//   digitalWrite(LedPin_1, LOW);
+
+//   delay (1000);
+
+
+//   tone(piezoPin, NOTE_F5, 1000);
+//   digitalWrite(LedPin_2, HIGH);
+
+//   delay (1000);
+
+//   noTone(piezoPin);
+//   digitalWrite(LedPin_2, LOW);
+
+//   delay (1000);
+
+
+// }
---
+++ testing.ino
@@ -0,0 +1,72 @@
+#include "pitches.h"
+
+constexpr int noteOne = NOTE_G3;
+constexpr int noteTwo = NOTE_D3;
+
+
+constexpr int LedPin_1 = 6;
+constexpr int LedPin_2 = 7;
+int lastLedUpdate = 0;
+constexpr int ledPinInterval = 1000;
+
+
+constexpr int piezoPin = 13;
+int lastPiezoUpdate = 0;
+constexpr int piezoPinInterval = 700;
+
+int notePlaying = 1;
+
+int Notes[4] = {NOTE_G3, NOTE_D3, NOTE_F6, NOTE_C1};
+int noteCount = 0;
+
+
+
+void setup() {
+  pinMode(LedPin_1, HIGH);
+  pinMode(LedPin_2, LOW);
+
+  // tone(piezoPin, NOTE_G3, 1000);
+  digitalWrite(LedPin_1, HIGH);
+  digitalWrite(LedPin_2, LOW);
+}
+
+void loop() {
+  const int currentMillis = millis();
+  if ((currentMillis - lastLedUpdate) >= ledPinInterval) {
+    const int ledState_1 = digitalRead(LedPin_1);
+    digitalWrite(LedPin_1, !ledState_1);
+    const int ledState_2 = digitalRead(LedPin_2);
+    digitalWrite(LedPin_2, !ledState_2);
+    lastLedUpdate = currentMillis;
+  }
+  // if ((currentMillis - lastPiezoUpdate) >= piezoPinInterval) {
+  //   if (notePlaying ==1) notePlaying =2;
+  //   else notePlaying = 1;
+
+  //   int noteToPlay = noteOne;
+  //   if(notePlaying ==2) noteToPlay = noteTwo;
+    
+  //   tone(piezoPin, noteToPlay, piezoPinInterval);
+
+
+  //   lastPiezoUpdate = currentMillis;
+  // }
+  if ((currentMillis - lastPiezoUpdate) >= piezoPinInterval) {
+  
+    int noteToPlay = Notes[noteCount];
+    
+    tone(piezoPin, noteToPlay, piezoPinInterval);
+
+    int NoteLen = sizeof(Notes)/sizeof(Notes[0]);
+
+    if (noteCount < NoteLen) {
+    noteCount = noteCount + 1;}
+    else 
+    noteCount = 0;
+    
+
+    lastPiezoUpdate = currentMillis;
+  }
+
+}
+
